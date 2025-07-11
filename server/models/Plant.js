af334e4ef5da1b5012afe2e2d52809922f99ae00
shrey savagave <shreysavagave@gmail.com>
2025-07-11 21:06:00 +0530

    changed mogel schema

--- server/models/Plant.js
+++ server/models/Plant.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 
 const plantSchema = new mongoose.Schema({
-  name: String,
-});
+  name: {
+    type: String,
+    required: true
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, { timestamps: true });
 
 module.exports = mongoose.model('Plant', plantSchema);
